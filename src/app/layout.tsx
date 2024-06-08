3511daf628b20e8af181f53a92e3aeea6c1a719b
johnymre <johnermy@gmail.com>
2024-06-08 20:38:40 +0800

    Final Portfolio Design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta property="og:image" content="johnermy.vercel.app/featuredimg.png" />
+      <meta property="og:image" content="/featuredimg.png" />
       </head>
       <body className={inter.className}>
       <Header />
